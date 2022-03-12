3fcf180ae070917149b1555cb0117c27f2fc8829
ruihou1 <66161995+ruihou1@users.noreply.github.com>
2022-03-12 14:19:29 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 #把标准化过程打包
-import joblib
+from sklearn.externals import joblib
 ss = joblib.load('E:/DIAPRE/文章最终结果数据/scalarsave')
 from tensorflow.keras.models import load_model
 model = load_model('E:/DIAPRE/文章最终结果数据/model.h6')
